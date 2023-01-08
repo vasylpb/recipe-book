38ee6f531be9e1f4d98a12369876da0c9c9d429a
vasylpb <vasylpb89@gmail.com>
2023-01-08 16:23:14 +0200

    routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,6 +12,12 @@ export class RecipeService {
       'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1513288060%2Fvegetarian-bolognese-1801-ck.jpg%3Fitok%3Dqzd9xdfa&w=800&c=sc&poi=face&q=60',
       [new Ingredient('Tomato', 2), new Ingredient('Basil', 4)]
     ),
+    new Recipe(
+      'A Test recipe 2',
+      'This is a simply test',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1513288060%2Fvegetarian-bolognese-1801-ck.jpg%3Fitok%3Dqzd9xdfa&w=800&c=sc&poi=face&q=60',
+      [new Ingredient('Tomato', 2), new Ingredient('Basil', 4)]
+    ),
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
@@ -20,6 +26,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
